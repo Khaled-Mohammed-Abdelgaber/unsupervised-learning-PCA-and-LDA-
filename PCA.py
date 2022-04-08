2753158f8fc32311fe63a4d000f4e4fa43d71f5c
Khaled-Mohammed-Abdelgaber <66785999+Khaled-Mohammed-Abdelgaber@users.noreply.github.com>
2022-04-08 02:34:10 +0200

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -19,14 +19,14 @@ from scipy.io import loadmat
 #-----------------------------------------------------------------------
 
 #  select random points
-#def init_centroids(X, k):
-#    m, n = X.shape
-#    centroids = np.zeros((k, n))
-#    idx = np.random.randint(0, m, k)
-#    
-#    for i in range(k):
-#        centroids[i,:] = X[idx[i],:]
-#    
+def init_centroids(X, k):
+  m, n = X.shape
+    centroids = np.zeros((k, n))
+    idx = np.random.randint(0, m, k)
+    
+    for i in range(k):
+        centroids[i,:] = X[idx[i],:]
+
 #    return centroids
 #
 #
